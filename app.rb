edfa43f6dc1634ed893904c60a8d0c80b8193dd4
niikz <i.k307112@gmail.com>
2021-08-02 00:08:03 +0900

    app.rbを作成

---
+++ app.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader'
+require 'json'
+
+get '/notes' do
+  @title = 'メモアプリ'
+  @notes = []
+  filename = Dir.glob('json/*').sort.reverse
+  filename.each do |file|
+    File.open(file, 'r') do |f|
+      @notes << JSON.parse(f.read, symbolize_names: true)
+    end
+  end
+
+  erb :notes
+end
+
+get '/notes/new' do
+  @title = '新しいメモを作成'
+  erb :new
+end
+
+post '/notes' do
+  id = Time.now.strftime('%Y%m%d%H%M%S')
+  title = params[:title]
+  comment = params[:comment]
+  File.open("json/#{id}.json", 'w') do |file|
+    hash = { id: id, title: title, comment: comment }
+    JSON.dump(hash, file)
+  end
+
+  redirect '/notes', 303
+end
+
+get '/notes/:id' do
+  @title = 'メモ内容を確認'
+  @id = params[:id]
+  File.open("json/#{@id}.json", 'r') do |file|
+    @notes = JSON.parse(file.read, symbolize_names: true)
+  end
+
+  erb :note
+end
+
+get '/notes/:id/edit' do
+  @title = 'メモ内容を編集'
+  @id = params[:id]
+  File.open("json/#{@id}.json", 'r') do |file|
+    @notes = JSON.parse(file.read, symbolize_names: true)
+  end
+
+  erb :edit
+end
+
+patch '/notes/:id' do
+  id = params[:id]
+  title = params[:title]
+  comment = params[:comment]
+  File.open("json/#{id}.json", 'w') do |file|
+    hash = { id: id, title: title, comment: comment }
+    JSON.dump(hash, file)
+  end
+
+  redirect '/notes', 303
+end
+
+delete '/notes/:id' do
+  id = params[:id]
+  File.delete("json/#{id}.json")
+
+  redirect '/notes', 303
+end
